671ff1c02890ea81560fa35323cc31a0432661f8
Bertalan Fodor <fodber@organum.hu>
2026-03-07 16:09:42 +0000

    fix: update queue driver to use Redis and configure Redis host

--- config/database.php
+++ config/database.php
@@ -75,7 +75,7 @@ return [
         'cluster' => false,
 
         'default' => [
-            'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'host'     => env('REDIS_HOST', 'redis'),
             'port'     => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
         ],
